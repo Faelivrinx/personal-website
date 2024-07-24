7017a88abcb8f1c9712e70eed4abf0ed4d96ed34
faelivrinx <jurasz.do@gmail.com>
2024-07-24 21:26:19 +0200

    Fix site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://faelivrinx.github.io",
+  site: "https://faelivrinx.github.io/personal-website/",
   base: "/",
   integrations: [mdx(), sitemap(), tailwind()],
 });
