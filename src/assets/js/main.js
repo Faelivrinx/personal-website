0bbb0778e47e88031e3f3d407209412db37f9822
faelivrinx <jurasz.do@gmail.com>
2024-07-24 21:06:27 +0200

    Finished first template of the site

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,16 @@
 /* eslint-disable */
 window.darkMode = false;
 
+const stickyClasses = ["fixed", "h-14"];
+const unstickyClasses = ["absolute", "h-20"];
+const stickyClassesContainer = [
+  "border-neutral-300/50",
+  "bg-white/80",
+  "dark:border-neutral-600/40",
+  "dark:bg-neutral-900/60",
+  "backdrop-blur-2xl",
+];
+const unstickyClassesContainer = ["border-transparent"];
 let headerElement = null;
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -16,6 +26,9 @@ document.addEventListener("DOMContentLoaded", () => {
     showDay();
   }
 
+  stickyHeaderFuncionality();
+  applyMenuItemClasses();
+  evaluateHeaderPosition();
   mobileMenuFunctionality();
 });
 
@@ -70,4 +83,40 @@ window.closeMobileMenu = () => {
   document.getElementById("openMenu").classList.remove("hidden");
   document.getElementById("menu").classList.add("hidden");
   document.getElementById("mobileMenuBackground").classList.add("hidden");
+};
+
+window.stickyHeaderFuncionality = () => {
+  window.addEventListener("scroll", () => {
+    evaluateHeaderPosition();
+  });
+};
+
+window.evaluateHeaderPosition = () => {
+  if (window.scrollY > 16) {
+    headerElement.firstElementChild.classList.add(...stickyClassesContainer);
+    headerElement.firstElementChild.classList.remove(
+      ...unstickyClassesContainer,
+    );
+    headerElement.classList.add(...stickyClasses);
+    headerElement.classList.remove(...unstickyClasses);
+    document.getElementById("menu").classList.add("top-[56px]");
+    document.getElementById("menu").classList.remove("top-[75px]");
+  } else {
+    headerElement.firstElementChild.classList.remove(...stickyClassesContainer);
+    headerElement.firstElementChild.classList.add(...unstickyClassesContainer);
+    headerElement.classList.add(...unstickyClasses);
+    headerElement.classList.remove(...stickyClasses);
+    document.getElementById("menu").classList.remove("top-[56px]");
+    document.getElementById("menu").classList.add("top-[75px]");
+  }
+};
+
+window.applyMenuItemClasses = () => {
+  const menuItems = document.querySelectorAll("#menu a");
+  for (let i = 0; i < menuItems.length; i++) {
+    if (menuItems[i].pathname === window.location.pathname) {
+      menuItems[i].classList.add("text-neutral-900", "dark:text-white");
+    }
+  }
+  //:class="{ 'text-neutral-900 dark:text-white': window.location.pathname == '{menu.url}', 'text-neutral-700 dark:text-neutral-400': window.location.pathname != '{menu.url}' }"
 };
