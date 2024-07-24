71bcba07528213fe6ca30603bc6df3ef58540d36
faelivrinx <jurasz.do@gmail.com>
2024-07-24 21:29:49 +0200

    Delete site configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://faelivrinx.github.io/personal-website/",
-  base: "/",
   integrations: [mdx(), sitemap(), tailwind()],
 });
