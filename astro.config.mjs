a4b0d55108a0aa84377763db5b237584479f6329
faelivrinx <jurasz.do@gmail.com>
2024-07-24 21:33:07 +0200

    Working with actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://faelivrinx.github.io/",
+  base: "personal-website",
   integrations: [mdx(), sitemap(), tailwind()],
 });
