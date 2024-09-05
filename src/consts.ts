c32129e4369faa543026014ae492080179ffebdd
faelivrinx <jurasz.do@gmail.com>
2024-09-05 09:00:05 +0200

    Metadata changed

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Blog";
-export const SITE_DESCRIPTION = "Dominik Jurasz - personal website";
+export const SITE_TITLE = "Dominik Jurasz";
+export const SITE_DESCRIPTION = "personal website of ambitious programmer";
